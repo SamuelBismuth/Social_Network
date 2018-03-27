1db983b6f77c748e288ba50090c02727658908b8
Samuel <sam101@hotmail.fr>
2018-03-27 12:45:33 +0300

    Adding comments, one issue about the followers is staying insolvable

--- include/Member.h
+++ include/Member.h
@@ -1,20 +1,27 @@
 #ifndef MEMBER_H
 #define MEMBER_H
 
+/** All the includes needed. */
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <iostream>
-#include <set>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
+/**
+ * \brief  Creation of the \class Member.
+ * A Member is able to follow and unfollow another member.
+ * \author Johann and Samuel.
+ */
 class Member {
 
+    /** All the public prototypes. */
     public:
-        Member();
-        virtual ~Member();
+        Member(); //Constructor.
+        virtual ~Member(); //Destructor.
         void follow(Member member);
         void unfollow(Member member);
         int numFollowers();
@@ -23,13 +30,16 @@ class Member {
 
     protected:
 
+    /** All the private prototypes. */
     private:
-        int followers  = 0,
-            followings = 0;
-        set<Member> listFollowers;
-        set<Member> listFollowings;
-        friend bool operator< (const Member &first, const Member &second);
-        static std::vector<Member*> listOfUsers;
+        int followers  = 0, //Variable.
+            followings = 0; //Variable.
+        vector<Member> listFollowers;  //List.
+        vector<Member> listFollowings;  //List.
+        static vector<Member*> listUsers; //List.
+        friend bool operator== (const Member &first, const Member &second);
+        inline bool containsUser(Member *user);
+        inline bool containsFollowing(Member following);
 };
 
 #endif // MEMBER_H
--- src/Member.cpp
+++ src/Member.cpp
@@ -1,50 +1,120 @@
+/**
+ * \brief  Implementation of the \class Member.
+ * A Member is able to follow and unfollow another member.
+ * \author Johann and Samuel.
+ */
+
+/** Include of the file Member.h". */
 #include "Member.h"
 
+/** We use a static counter to count the number of user(s) for one reason : the complexity.
+ * Another solution would be to return the size of the array but there are two problems :
+ * which is more important : complexity or memory ??
+ * And so, the members declared as global are not in the list ?
+ */
 static int numberOfUsers = 0;
-std::vector<Member*> Member::listOfUsers;
 
+/** A static vector which contains all the user of the social network. */
+vector<Member*> Member::listUsers;
+
+
+/**
+ * \brief Constructor to control all the new members.
+ *  Since the duplicate follow has no effect, we check \if the follow is a new one or not.
+ */
 Member::Member() {
-    if(std::find(listOfUsers.begin(), listOfUsers.end(), this) == listOfUsers.end()) {
-        listOfUsers.push_back(this);
+    if(!containsUser(this)) {
+        listUsers.push_back(this);
         numberOfUsers++;
     }
 }
 
+/**
+ * \brief Destructor to control all the delete members.
+ * One of the issues was in the use of the destructor : we saw that the destructor was called numerous times, and this without our control.
+ * So we're now checking \if the user is in the list or not.
+ */
 Member::~Member() {
-    if(std::find(listOfUsers.begin(), listOfUsers.end(), this) != listOfUsers.end())
+    if(containsUser(this)) {
+        listUsers.erase(remove(listUsers.begin(), listUsers.end(), this), listUsers.end());
         numberOfUsers--;
+    }
 }
 
+/**
+ * \param member.
+ * \brief this method do a "follow" between from the "this" to the member.
+ * We have an issue here which is that when we want to bring some modifications to the member (as a parameter) anything happen.
+ */
 void Member::follow(Member member) {
-    if(listFollowings.count(member) == 0) {
+    if(!containsFollowing(member)) {
         followings++;
-        listFollowings.insert(member);
-        //
-        //
+        listFollowings.push_back(member);
+        //from here we have an issue
+        member.followers++;
+        member.listFollowers.push_back(*this);
+        //to here
     }
 }
 
+/**
+ * \param member.
+ * \brief this method do a "unfollow" between from the "this" to the member.
+ * We have an issue here which is that when we want to bring some modifications to the member (as a parameter) anything happen.
+ */
 void Member::unfollow(Member member) {
-    if(listFollowings.count(member) == 1) {
+    if(containsFollowing(member)) {
         followings--;
-        listFollowings.erase(member);
-        //member.decreaseNumFollowers();
-        //member.listFollowers.erase(member);
+        listFollowings.erase(remove(listFollowings.begin(), listFollowings.end(), member), listFollowings.end());
+        //from here we have an issue
+        member.followers--;
+        member.listFollowers.erase(remove(listFollowings.begin(), listFollowings.end(), *this), listFollowings.end());
+        //to here
     }
 }
 
+/**
+ * \return the number of user of the social network.
+ */
 int Member::count() {
     return numberOfUsers;
 }
 
+/**
+ * \return the number of follower of the "this" member.
+ */
 int Member::numFollowers() {
     return followers;
 }
 
+/**
+ * \return the number of following of the "this" member.
+ */
 int Member::numFollowing() {
     return followings;
 }
 
-bool operator< (const Member &first, const Member &second) {
-    return first.followers < second.followers;
+/**
+ * \param Member *user.
+ * \return true \if the parameter is one of the user of the social network else return false.
+ */
+bool Member::containsUser(Member *user) {
+    return find(listUsers.begin(), listUsers.end(), user) != listUsers.end();
+}
+
+/**
+ * \param Member following.
+ * \return true \if the parameter is one of the following of the "this" member.
+ */
+bool Member::containsFollowing(Member following) {
+    return find(listFollowings.begin(), listFollowings.end(), following) != listFollowings.end();
+}
+
+/**
+ * \param Member first and second.
+ * \return true if there are equal, false if they don't.
+ * \brief This method define the use of the operator "==".
+ */
+bool operator== (const Member &first, const Member &second) {
+    return first.followers == second.followers;
 }
